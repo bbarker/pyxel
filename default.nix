a64273df8e2dcb2e82e0a8c3dd67ef715e8b2de8
Brandon Elam Barker <beb82@cornell.edu>
2018-09-07 23:57:06 +0000

    WIP: virtualenv not found

---
+++ default.nix
@@ -0,0 +1,48 @@
+#
+# This file is just a script to setup the environment using Nix and is not needed
+# unless you are using Nix in some capacity; For more information see https://nixos.org/nix/ and
+# https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/python.md
+#
+#
+# To use, run `nix-shell` from this directory, or `nix-shell default.nix`.
+#
+
+with import <nixpkgs> {};
+with pkgs.python36Packages;
+let pyBasePkg = stdenv.mkDerivation {
+  name = "impurePythonEnv";
+
+  buildInputs = [
+    glibcLocales # for character support (unicode etc)
+    glfw
+    zlib  
+  ];
+};
+in (python36.buildEnv.override {
+  postBuild = ''
+    # set SOURCE_DATE_EPOCH so that we can use python wheels
+    SOURCE_DATE_EPOCH=$(date +%s)
+    export LANG=en_US.UTF-8
+    virtualenv venv
+    source venv/bin/activate
+    export LD_LIBRARY_PATH=${glfw}/lib:$LD_LIBRARY_PATH
+    pip install glfw
+    python setup.py develop
+    export PATH=$PWD/venv/bin:$PATH
+    export PYTHONPATH=$PWD
+  '';
+  
+    #
+    # these packages are required for virtualenv and pip to work:
+    #
+  extraLibs = [    
+    pyBasePkg
+    python36Full
+    python36Packages.cffi
+    python36Packages.virtualenv
+    python36Packages.pip
+    python36Packages.pillow
+
+  ];
+}).env
+
